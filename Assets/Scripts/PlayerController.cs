3e8f80ffd3d1d2881bb94abe800c3a2e9de1b90e
jordi <jordikiter@hotmail.es>
2021-01-13 23:40:50 +0000

    First Commit FPS game using unity
    First Commit FPS game using unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor playerMotor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate movement velocity
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = xMov * transform.right;
+        Vector3 movVertical = zMov * transform.forward;
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+        //playerMotor.Move(velocity);
+    }
+}
